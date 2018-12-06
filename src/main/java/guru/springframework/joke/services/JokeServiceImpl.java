1186fe074b23a38bd4544306dac619fc79f8b24a
dario <me@example.com>
2018-12-06 23:26:33 +0100

    added Configuration class

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -4,15 +4,14 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokeServiceImpl implements  JokeService {
+public class JokeServiceImpl implements JokeService {
 
-//Creo variabile final cosi spring la istanzia una sola volta nel costruttore
+    //Creo variabile final cosi spring la istanzia una sola volta nel costruttore
     //tramite la constructor injection
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override

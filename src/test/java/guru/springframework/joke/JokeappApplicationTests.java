2de9aff0179466115f4f128cfa6e733e050026e9
dario <me@example.com>
2018-12-06 23:50:51 +0100

    added XML Configuration

--- src/test/java/guru/springframework/joke/JokeappApplicationTests.java
+++ src/test/java/guru/springframework/joke/JokeappApplicationTests.java
@@ -3,10 +3,12 @@ package guru.springframework.joke;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplicationTests {
 
     @Test
